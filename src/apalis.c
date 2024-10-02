a3c593731aeb84a11d26d5aa22e94b9bfe425dab
cetio <stakeandtake39@gmail.com>
2024-10-02 04:43:24 -0400

    bizarre errors

--- src/apalis.c
+++ src/apalis.c
@@ -4,64 +4,11 @@
 #include <linux/binfmts.h>
 #include <linux/fs_struct.h>
 #include <linux/string.h>
+#include <linux/security.h>
 
 MODULE_LICENSE("GPL");
 
-#define NUM_IVFS 4
-#define NUM_BINMD 3
-
-struct ivfs_descriptor
-{
-    struct kprobe kp[NUM_IVFS];
-
-    int (*open)(const struct path*, struct file*, const struct cred*);
-    ssize_t (*write)(struct file*, const char *, size_t, loff_t*);
-    ssize_t (*read)(struct file*, const char*, size_t, loff_t*);
-    int (*readdir)(struct file*, struct dir_context*);
-};
-
-struct binmd_descriptor
-{
-    struct kprobe kp[NUM_BINMD];
-
-    int (*execve)(struct linux_binprm*);
-    int (*usb_probe)(struct device*);
-    int (*bus_probe)(struct device*);
-};
-
-int ivfs_init(struct ivfs_descriptor* desc)
-{
-    for (int i = 0; i < NUM_IVFS; i++)
-    {
-        if ((desc->kp[i]).symbol_name == NULL)
-            continue;
-            
-        int err = register_kprobe(&(desc->kp[i]));
-
-        if (err != 0)
-            return err;
-    }
-    
-    return 0;
-}
-
-int binmd_init(struct binmd_descriptor* desc)
-{
-    for (int i = 0; i < NUM_BINMD; i++)
-    {
-        if ((desc->kp[i]).symbol_name == NULL)
-            continue;
-            
-        int err = register_kprobe(&(desc->kp[i]));
-
-        if (err != 0)
-            return err;
-    }
-    
-    return 0;
-}
-
-static int bprm_handler_pre(struct kprobe* p, struct pt_regs* regs)
+static int _pre_alloc_bprm(struct kprobe* p, struct pt_regs* regs)
 {
     struct linux_binprm* bin = (struct linux_binprm*)regs->di;
     
@@ -72,26 +19,40 @@ static int bprm_handler_pre(struct kprobe* p, struct pt_regs* regs)
     return 0;
 }
 
-static int fs_handler_pre(struct kprobe* p, struct pt_regs* regs)
+static int _pre_getname_flags(struct kprobe* p, struct pt_regs* regs)
 {
     const char __user* filename = (const char __user*)regs->di;
     char kname[PATH_MAX];
     int len = strncpy_from_user(kname, filename, PATH_MAX);
 
-    if (len == 0 || regs->si == 1)
+    // If there's no data or RDX is 0 then this isn't actually a path that we care about (yet.)
+    if (len == 0 || regs->dx != 0)
         return 0;
 
-    printk(KERN_INFO "FS-H DI: %s SI: %d DX: %d\n", kname, regs->si, regs->dx);
+    // Absolute paths only.
+    if (strcmp(kname, "/home/cet/.zshrc") == 0)
+        return -1;
+
+    printk(KERN_INFO "getname_flags | DI: %s SI: %d DX: %d\n", kname, regs->si, regs->dx);
     return 0;
 }
 
-static int vfs_handler_pre(struct kprobe* p, struct pt_regs* regs)
+static int _pre_iterate_dir(struct kprobe* p, struct pt_regs* regs)
 {
-    // char name[256];
-    // char* ret = d_path((struct path*)regs->di, name, sizeof(name));
+    struct file* file = (struct file*)regs->di;
+    struct dir_context* ctx = (struct dir_context*)regs->si;
 
-    // printk(KERN_INFO "VFS-H: %s\n", ret);
-    printk(KERN_INFO "gaah");
+    if (file == NULL || file->f_path.dentry == NULL || security_file_permission(file, MAY_READ))
+        return 0;
+
+    if (ctx == NULL)
+        return 0;
+    
+    struct path path = file->f_path;
+    char buf[PATH_MAX];
+    char* name = dentry_path(path.dentry, buf, sizeof(buf));
+
+    printk(KERN_INFO "iterate_dir | file: %s", name);
     return 0;
 }
 
@@ -114,13 +75,13 @@ static int __init apalis_init(void)
     }
 
     // kp.symbol_name = "getname_flags";
-    // kp.pre_handler = fs_handler_pre;
-    kp.symbol_name = "vfs_iter_read";
-    kp.pre_handler = vfs_handler_pre;
+    // kp.pre_handler = _pre_getname_flags;
+    kp.symbol_name = "iterate_dir";
+    kp.pre_handler = _pre_iterate_dir;
     int err = register_kprobe(&kp);
 
-    if (err < 0)
-        printk(KERN_ERR "Failed to register file system handler with error %d!\n", err);
+    if (err != 0)
+        printk(KERN_ERR "Failed to register getname_flags handler with error %d!\n", err);
 
     printk(KERN_INFO "Finalized load sequence.\n");
     return 0;
@@ -133,4 +94,4 @@ static void __exit apalis_exit(void)
 }
 
 module_init(apalis_init);
-module_exit(apalis_exit);
+module_exit(apalis_exit);;
